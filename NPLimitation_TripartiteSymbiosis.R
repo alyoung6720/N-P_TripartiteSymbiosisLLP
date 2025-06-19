43e4413f5bdbcf857adf16325b67fd8d61235e91
alyoung6720 <60701103+alyoung6720@users.noreply.github.com>
2025-06-19 16:11:22 -0400

    Updated mixed models

--- NPLimitation_TripartiteSymbiosis.R
+++ NPLimitation_TripartiteSymbiosis.R
@@ -38,11 +38,11 @@ Nfix <- read.csv("Field_GClog.csv") %>%
 LeafNuts <- read.csv("Field_LeafNutContent.csv")
 
 data1 <- merge(Nfix, BiomassNoduleAMF, by=c("Site", "Treatment", "Plant_ID"), all=T)
-data2 <- merge(data1, LeafNuts, by=c("Site", "Treatment", "Plant_ID", "Replicate"), all=T)
+data2 <- merge(data1, LeafNuts, by=c("Site", "Treatment", "Plant_ID", "Group"), all=T)
 
 # CHANGE SITE, REPLICATE, AND TREATMENT TO FACTOR #
 data2$Site <- as.factor(data2$Site)
-data2$Replicate <- as.factor(data2$Replicate)
+data2$Group <- as.factor(data2$Group)
 data2$Treatment <- as.factor(data2$Treatment)
 
 
@@ -52,36 +52,36 @@ data2$Treatment <- as.factor(data2$Treatment)
 
 ## CHECK FOR NORMALITY, ETC ####
 hist(data$ANPP)
-res_ANPP <- lmer(ANPP ~ Treatment + (1|Site/Replicate), data = data)
+res_ANPP <- lmer(ANPP ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resanpp <- residuals(res_ANPP, type="pearson")
 plot(resanpp)
 qqnorm(resanpp)
 simulateResiduals(fittedModel = res_ANPP, plot = TRUE)
 ols_test_normality(resanpp)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(ANPP ~ interaction(Treatment), data = data)
+result = leveneTest(ANPP ~ interaction(Treatment), data = data2)
 print(result)
 
 # Shapiro-Wilk, Kolmogorov-Smirnov, and Anderson-Darling tests all improved after transformation #
-res_ANPP <- lmer(sqrt(ANPP) ~ Treatment + (1|Site/Replicate), data = data)
+res_ANPP <- lmer(sqrt(ANPP) ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resanpp <- residuals(res_ANPP, type="pearson")
 plot(resanpp)
 qqnorm(resanpp)
 simulateResiduals(fittedModel = res_ANPP, plot = TRUE)
 ols_test_normality(resanpp)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(sqrt(ANPP)~ interaction(Treatment), data = data)
+result = leveneTest(sqrt(ANPP)~ interaction(Treatment), data = data2)
 print(result)
 
 
 # RUN MODEL, GET R2 AND PAIRWISE SIGNIFICANT DIFFERENCES #
-a <- lmer(sqrt(ANPP) ~ Treatment + (1|Site/Replicate), data = anpp)
+a <- lmer(sqrt(ANPP) ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 anova(a)
 r.squaredGLMM(a)
 emmeans(a, pairwise ~ Treatment, adjust="BH") 
 
 
-ggplot(data=barGraphStats(data=anpp, variable="ANPP",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
+ggplot(data=barGraphStats(data=data2, variable="ANPP",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
   geom_bar(stat='identity', position="dodge", width=0.75) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, size=1, position=position_dodge(0.9)) +
   ylab("Shoot Biomass (g)") +
@@ -102,18 +102,18 @@ ggplot(data=barGraphStats(data=anpp, variable="ANPP",byFactorNames=c("Treatment"
 
 ## CHECK FOR NORMALITY, ETC ####
 hist(data$NodNum)
-res_Nod <- lmer(NodNum ~ Treatment + (1|Site/Replicate), data = data)
+res_Nod <- lmer(NodNum ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resnod <- residuals(res_Nod, type="pearson")
 plot(resnod)
 qqnorm(resnod)
 simulateResiduals(fittedModel = res_Nod, plot = TRUE)
 ols_test_normality(resnod)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(NodNum ~ interaction(Treatment), data = anpp)
+result = leveneTest(NodNum ~ interaction(Treatment), data = anpp2)
 print(result)
 
 # Shapiro-Wilk, Kolmogorov-Smirnov, and Anderson-Darling tests all improved after transformation #
-res_Nod <- lmer(log1p(NodNum) ~ Treatment + (1|Site/Replicate), data = data)
+res_Nod <- lmer(log1p(NodNum) ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resnod <- residuals(res_Nod, type="pearson")
 plot(resnod)
 qqnorm(resnod)
@@ -121,18 +121,18 @@ shapiro.test(resnod)
 simulateResiduals(fittedModel = res_Nod, plot = TRUE)
 ols_test_normality(resnod)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(log1p(NodNum)~ interaction(Treatment), data = data)
+result = leveneTest(log1p(NodNum)~ interaction(Treatment), data = data2)
 print(result)
 
 
 # RUN MODEL, GET R2 AND PAIRWISE SIGNIFICANT DIFFERENCES #
-b <- lmer(log1p(NodNum) ~ Treatment + (1|Site/Replicate), data = data)
+b <- lmer(log1p(NodNum) ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 anova(b)
 r.squaredGLMM(b)
 emmeans(b, pairwise ~ Treatment, adjust="BH") 
 
 
-ggplot(data=barGraphStats(data=data, variable="NodNum",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
+ggplot(data=barGraphStats(data=data2, variable="NodNum",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
   geom_bar(stat='identity', position="dodge", width=0.75) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, size=1, position=position_dodge(0.9)) +
   ylab("Nodule Number") +
@@ -154,25 +154,25 @@ ggplot(data=barGraphStats(data=data, variable="NodNum",byFactorNames=c("Treatmen
 
 ## CHECK FOR NORMALITY, ETC ####
 hist(data$PercMC)
-res_MC <- lmer(PercMC ~ Treatment + (1|Site/Replicate), data = data)
+res_MC <- lmer(PercMC ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resmc <- residuals(res_MC, type="pearson")
 plot(resmc)
 qqnorm(resmc)
 ols_test_normality(resmc)
 simulateResiduals(fittedModel = res_MC, plot = TRUE)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(PercMC ~ interaction(Treatment), data = data)
+result = leveneTest(PercMC ~ interaction(Treatment), data = data2)
 print(result)
 
 
 # RUN MODEL, GET R2 AND PAIRWISE SIGNIFICANT DIFFERENCES #
-c <- lmer(PercMC ~ Treatment + (1|Site/Replicate), data = data)
+c <- lmer(PercMC ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 anova(c)
 r.squaredGLMM(c)
 emmeans(c, pairwise ~ Treatment, adjust="BH")
 
 
-ggplot(data=barGraphStats(data=data, variable="PercMC",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
+ggplot(data=barGraphStats(data=data2, variable="PercMC",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
   geom_bar(stat='identity', position="dodge", width=0.75) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, size=1, position=position_dodge(0.9)) +
   ylab("% AMF Root Colonization") +
@@ -191,9 +191,10 @@ ggplot(data=barGraphStats(data=data, variable="PercMC",byFactorNames=c("Treatmen
 #################################
 # Leaf Nutrient Content #
 
+# Nitrogen #
 ## CHECK FOR NORMALITY, ETC ####
 hist(data2$N)
-res_N <- lmer(N ~ Treatment + (1|Site/Replicate), data = data2)
+res_N <- lmer(N ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resN <- residuals(res_N, type="pearson")
 plot(resN)
 qqnorm(resN)
@@ -203,23 +204,12 @@ ols_test_normality(resN)
 result = leveneTest(N ~ interaction(Treatment), data = data2)
 print(result)
 
-# Shapiro-Wilk, Kolmogorov-Smirnov, and Anderson-Darling tests all improved after transformation #
-res_N <- lmer(log(N) ~ Treatment + (1|Site/Replicate), data = data2)
-resN <- residuals(res_N, type="pearson")
-plot(resN)
-qqnorm(resN)
-simulateResiduals(fittedModel = res_N, plot = TRUE)
-ols_test_normality(resN)
-# Using leveneTest() to test for homoscedasticity
-result = leveneTest(log(N) ~ interaction(Treatment), data = data2)
-print(result)
-
 
 # RUN MODEL, GET R2 AND PAIRWISE SIGNIFICANT DIFFERENCES #
-a <- lmer(log(N) ~ Treatment + (1|Site/Replicate), data = data2)
-anova(a)
-r.squaredGLMM(a)
-emmeans(a, pairwise ~ Treatment, adjust="BH") 
+d <- lmer(N ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
+anova(d)
+r.squaredGLMM(d)
+emmeans(d, pairwise ~ Treatment, adjust="BH") 
 
 
 ggplot(data=barGraphStats(data=data2, variable="N",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
@@ -238,40 +228,86 @@ ggplot(data=barGraphStats(data=data2, variable="N",byFactorNames=c("Treatment"))
 # EXPORT 1400 x 1400 #
 
 
+# Phosphorus #
+## CHECK FOR NORMALITY, ETC ####
+hist(data2$P)
+res_P <- lmer(P ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
+resP <- residuals(res_N, type="pearson")
+plot(resP)
+qqnorm(resP)
+simulateResiduals(fittedModel = res_P, plot = TRUE)
+ols_test_normality(resP)
+# Using leveneTest() to test for homoscedasticity
+result = leveneTest(P ~ interaction(Treatment), data = data2)
+print(result)
+
+# Shapiro-Wilk, Kolmogorov-Smirnov, and Anderson-Darling tests all improved after transformation #
+res_P <- lmer(log(P) ~ Treatment + (1|Site/Replicate), data = data2)
+resP <- residuals(res_P, type="pearson")
+plot(resP)
+qqnorm(resP)
+simulateResiduals(fittedModel = res_P, plot = TRUE)
+ols_test_normality(resP)
+# Using leveneTest() to test for homoscedasticity
+result = leveneTest(log(P) ~ interaction(Treatment), data = data2)
+print(result)
+
+
+# RUN MODEL, GET R2 AND PAIRWISE SIGNIFICANT DIFFERENCES #
+e <- lmer(P ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
+anova(e)
+r.squaredGLMM(e)
+emmeans(e, pairwise ~ Treatment, adjust="BH") 
+
+
+ggplot(data=barGraphStats(data=data2, variable="P",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
+  geom_bar(stat='identity', position="dodge", width=0.75) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, size=1, position=position_dodge(0.9)) +
+  ylab("Leaf Phosphorus Content (%)") +
+  xlab("Treatment") +
+  annotate("text", x= 1, y = 0.12, label= "a", size = 15)+ 
+  annotate("text", x= 2, y = 0.115, label= "a", size = 15)+ 
+  annotate("text", x= 3, y = 0.18, label= "b", size = 15)+ 
+  scale_fill_manual(values = c("#C9B793", "#6E6C81", "#93AD90")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        text = element_text(size = 40),axis.text.x=element_text(size = 30), 
+        legend.position="none",axis.text.y=element_text(size = 40))
+# EXPORT 1400 x 1400 #
+
 ##########################
 # N-Fixation #
 
 ## CHECK FOR NORMALITY, ETC ####
-hist(data$AvgEthPPM)
-res_fix <- lmer(AvgEthPPM ~ Treatment + (1|Site/Replicate), data = data)
+hist(data2$AvgEthPPM)
+res_fix <- lmer(AvgEthPPM ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resfix <- residuals(res_fix, type="pearson")
 plot(resfix)
 qqnorm(resfix)
 simulateResiduals(fittedModel = res_fix, plot = TRUE)
 ols_test_normality(resfix)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(AvgEthPPM ~ interaction(Treatment), data = data)
+result = leveneTest(AvgEthPPM ~ interaction(Treatment), data = data2)
 print(result)
 
 
-res_fix <- lmer(sqrt(AvgEthPPM) ~ Treatment + (1|Site/Replicate), data = data)
+res_fix <- lmer(sqrt(AvgEthPPM) ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
 resfix <- residuals(res_fix, type="pearson")
 plot(resfix)
 qqnorm(resfix)
 simulateResiduals(fittedModel = res_fix, plot = TRUE)
 ols_test_normality(resfix)
 # Using leveneTest() to test for homoscedasticity
-result = leveneTest(sqrt(AvgEthPPM) ~ interaction(Treatment), data = data)
+result = leveneTest(sqrt(AvgEthPPM) ~ interaction(Treatment), data = data2)
 print(result)
 
 
-d <- lmerTest::lmer(sqrt(AvgEthPPM) ~ Treatment + (1|Site), data = subset(data, NodNum<90))
-anova(d)
-r.squaredGLMM(d)
-emmeans(d, pairwise ~ Treatment, adjust="BH")
+e <- lmer(sqrt(AvgEthPPM) ~ Treatment + (1|Site) + (1|Site:Group), data = data2)
+anova(e)
+r.squaredGLMM(e)
 
 
-ggplot(data=barGraphStats(data=subset(data, NodNum<90), variable="AvgEthPPM",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
+ggplot(data=barGraphStats(data=subset(data2, NodNum<90), variable="AvgEthPPM",byFactorNames=c("Treatment")),aes(x=Treatment, y=mean, fill=Treatment)) +
   geom_bar(stat='identity', position="dodge", width=0.75) +
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, size=1, position=position_dodge(0.9)) +
   ylab("Ethylene Produced (ppm)") +
